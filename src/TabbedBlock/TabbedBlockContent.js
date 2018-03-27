f64725d5448eb3cb63ece5fe2cf477f144cbb2e8
gloss-water <gloss.water@gmail.com>
2018-03-27 01:05:10 -0600

    support image embed

--- src/TabbedBlock/TabbedBlockContent.js
+++ src/TabbedBlock/TabbedBlockContent.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const TabbedBlockContent = ({ data }) => (
-    <div className='tabbed-block-content'
-        dangerouslySetInnerHTML={{ __html: data.text }} />
+    <div className='tabbed-block-content'>
+        {
+            data.text ?
+                <div dangerouslySetInnerHTML={{ __html: data.text }} /> :
+                null
+        }
+        {
+            data.image ?  
+                <div>
+                    <img src={data.image} alt=''/>
+                </div> :
+                null
+        }
+    </div>
 );
 
 export default TabbedBlockContent;
