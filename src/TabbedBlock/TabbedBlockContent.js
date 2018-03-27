61ffa6868650a7221eb281f86004744835baa3c1
gloss-water <gloss.water@gmail.com>
2018-03-27 01:13:22 -0600

    add short animations for switching between tabs

--- src/TabbedBlock/TabbedBlockContent.js
+++ src/TabbedBlock/TabbedBlockContent.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { CSSTransitionGroup } from 'react-transition-group'
 
 const TabbedBlockContent = ({ data }) => (
-    <div className='tabbed-block-content'>
-        {
-            data.text ?
-                <div dangerouslySetInnerHTML={{ __html: data.text }} /> :
-                null
-        }
-        {
-            data.image ?  
-                <div>
-                    <img src={data.image} alt=''/>
-                </div> :
-                null
-        }
-    </div>
+    <CSSTransitionGroup
+          transitionName='tabbed-block-content-animation'
+          transitionEnterTimeout={300}
+          transitionLeave={false}
+    >
+        <div key={`content${data.header}`} className='tabbed-block-content'>
+            {
+                data.text ?
+                    <div dangerouslySetInnerHTML={{ __html: data.text }} /> :
+                    null
+            }
+            {
+                data.image ?  
+                    <div>
+                        <img src={data.image} alt=''/>
+                    </div> :
+                    null
+            }
+        </div>
+    </CSSTransitionGroup>
+    
 );
 
 export default TabbedBlockContent;
